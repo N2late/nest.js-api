b4407c7314b23f3cbfa56b53e46338fd404e93d1
Tiago Serra <35219940+N2late@users.noreply.github.com>
2023-01-12 16:03:54 +0100

    Implement nest config module: add to app.module with a global scope; add to prismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist';
 import { PrismaClient } from '@prisma/client';
 
 // provides a way to interact with a database using the Prisma API
 @Injectable()
 export class PrismaService extends PrismaClient {
-  // Call the parent class's constructor method with the options
-  constructor() {
+  /* Calling the parent class's constructor method with the options. */
+  constructor(config: ConfigService) {
     // Call the parent class's constructor method with the options
     super({
       datasources: {
         db: {
-          url: 'postgresql://nestapi:nestapi@localhost:5434/nestapi?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
